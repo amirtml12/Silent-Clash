89d490a8b119860a5826cb0409440065ec1d1025
amirtml12 <165264892+amirtml12@users.noreply.github.com>
2025-12-18 16:44:06 +0330

    MenuInGame

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -6,29 +6,28 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public GameObject PlayerController;
-   
-    
 
 
-    void OnCollisionEnter2D(Collision2D other)
+
+
+   void OnCollisionEnter2D(Collision2D other)
 {
-   
-
     if (other.gameObject.CompareTag("Player"))
     {
-        PlayerController pc = other.gameObject.GetComponent<PlayerController>();
-        
-        if (pc != null)
-        {
-            pc.TakeDamage(1);
-            EnemyController.instance.TakeDamage(2);
-            
-        
-
-        }
-
-        Destroy(gameObject);
+        PlayerController2.instance.TakeDamage(1);
     }
+    else if (other.gameObject.CompareTag("Enemy"))
+    {
+        EnemyController enemy = other.gameObject.GetComponent<EnemyController>();
+
+        if (enemy != null)
+        {
+            enemy.TakeDamage(2);
+        }
+    }
+
+    Destroy(gameObject);
 }
+
 
 }
